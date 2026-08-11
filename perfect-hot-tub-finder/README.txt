36bb2c2e310a64a2e5ce8edceb0f604dd39214d4
wpsoheltanvir <wpsoheltanvir@users.noreply.github.com>
2026-08-12 00:58:20 +0600

    Rename series delight widget

--- perfect-hot-tub-finder/README.txt
+++ perfect-hot-tub-finder/README.txt
@@ -48,6 +48,9 @@ define( 'PHTF_GITHUB_TOKEN', 'github_pat_your_token_here' );
 
 Changelog
 ---------
+= 1.0.193 =
+* Renamed the Hollywood Spa Series Slide Elementor widget to Hollywood Spa Series Delight.
+
 = 1.0.192 =
 * Renamed the Hollywood Spa Series Slider Elementor widget to Hollywood Series Hero.
 
--- perfect-hot-tub-finder/widgets/class-phtf-spa-series-delight-widget.php
+++ perfect-hot-tub-finder/widgets/class-phtf-spa-series-delight-widget.php
@@ -17,7 +17,7 @@ class PHTF_Spa_Series_Delight_Widget extends \Elementor\Widget_Base {
 	}
 
 	public function get_title() {
-		return esc_html__( 'Hollywood Spa Series Slide', 'perfect-hot-tub-finder' );
+		return esc_html__( 'Hollywood Spa Series Delight', 'perfect-hot-tub-finder' );
 	}
 
 	public function get_icon() {
